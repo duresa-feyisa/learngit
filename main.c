bb87826bee33670b0e4779851b9a9953a81eb688
ethio <ethi@gmail.com>
2021-05-28 04:05:36 +0300

    working list

--- main.c
+++ main.c
@@ -11,5 +11,6 @@ int main()
    printf("With precision specifier: %2.8d%-2.8d h\n", int_num, int_num);
    printf("With precision specifier: %10.2f %-10.2f h\n", flt_num, flt_num);
    printf("%d\n", 15);
+   printf("%d", 55);
   return 0;
 }
