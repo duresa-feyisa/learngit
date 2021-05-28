2b00aca1d72da6ffb683693015ccd631031f0b38
ethio <ethi@gmail.com>
2021-05-28 04:02:20 +0300

    first file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int int_num;
+   double flt_num;
+
+   int_num = 123;
+   flt_num = 123.456789;
+   printf("With precision specifier: %2.8d%-2.8d h\n", int_num, int_num);
+   printf("With precision specifier: %10.2f %-10.2f h\n", flt_num, flt_num);
+   printf("%d\n", 15);
+  return 0;
+}
